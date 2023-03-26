e2327c3bc032bc6ad729ba2ad060f28d4ff9c0d6
MoonLightDB <leagueofdato@gmail.com>
2023-03-26 13:16:45 +0400

    davaleba true/false

---
+++ davaleba 3/davaleba3.py
@@ -0,0 +1,21 @@
+
+name = "Dato"
+surname = "Bregvadze"
+age = "23"
+height = 180
+knows_programming = False
+
+print(name + " " + surname + " " "aris" " " + age + " " "wlis") 
+
+
+
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+
+print(name + age)
